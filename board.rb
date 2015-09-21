5125624c01bb6f2813723d509bc7ab4cf03f2b76
kushkp <ankushp89@gmail.com>
2015-09-20 17:28:00 -0700

    diagonals

--- board.rb
+++ board.rb
@@ -1,6 +1,6 @@
 class Board
   def initialize(cols, height, num_to_win)
-    @cols = cols
+    @num_cols = cols
     @height = height
     @grid = Array.new(cols) { Array.new(height) { :O } }
   end
@@ -17,9 +17,37 @@ class Board
   end
 
   def winner
-    
+    check_lines(@grid) ||
+      check_lines(@grid.transpose) ||
+      diagonal_winner
   end
 
 private
-  attr_reader :cols, :height
+  attr_reader :num_cols, :height
+
+  def diagonal_winner
+    check_diagonal(true) || check_diagonal(false)
+  end
+
+  def check_diagonal(pos)
+    #if pos = true => check positive slope diagonal /
+    #if pos = false => check negative slope diagonal \
+    start_col = pos ? 0 : (num_cols - 1)
+    diagonals = []
+    start_locs = (0...num_cols).map { |col| [0, col] }
+    start_locs += (1...height).map { |row| [row, start_col] }
+
+    start_locs.each do |row, col|
+      diagonal = []
+      while on_board?(row, col)
+        diagonal << @grid[row][col]
+        row += 1
+        col += pos ? 1 : -1
+      end
+      diagonals << diagonal
+    end
+
+    check_lines(diagonals)
+  end
+
 end

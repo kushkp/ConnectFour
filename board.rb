90725b95a00ba0d281e4ae28ef4f021e0c8908ae
kushkp <ankushp89@gmail.com>
2015-09-20 17:03:43 -0700

    drop disc

--- board.rb
+++ board.rb
@@ -1,21 +1,25 @@
 class Board
   def initialize(cols, height, num_to_win)
+    @cols = cols
+    @height = height
     @grid = Array.new(cols) { Array.new(height) { :O } }
   end
 
-  def drop_disc(player, col)
-
-  end
-
-  def col()
-
+  def drop_disc(disc, col)
+    curr_height = col_height(col)
+    return false if curr_height == height
+    row_idx = row - curr_height - 1
+    @grid[row_idx][col] = disc
   end
 
   def over?
-
+    !!winner
   end
 
   def winner
-
+    
   end
+
+private
+  attr_reader :cols, :height
 end
